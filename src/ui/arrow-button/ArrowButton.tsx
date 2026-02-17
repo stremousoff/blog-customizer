53dbb0c76df2e97a0f258d4aea9472918659e028
Anton Stremousov <stremousoff@gmail.com>
2026-02-17 23:26:06 +0300

    fix import

--- src/ui/arrow-button/ArrowButton.tsx
+++ src/ui/arrow-button/ArrowButton.tsx
@@ -3,7 +3,6 @@ import arrow from 'src/images/arrow.svg';
 import styles from './ArrowButton.module.scss';
 import clsx from 'clsx';
 import { Fragment } from 'react';
-import { ArticleParamsForm } from 'components/article-params-form';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
